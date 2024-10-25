4ee0efab8380ff1748e32285a072d00c98b7ce73
Samy ben beya <samyben2001@hotmail.com>
2024-10-25 14:51:01 +0200

    add loaddata during container start

--- PopPlay/dockerCmd.sh
+++ PopPlay/dockerCmd.sh
@@ -1,4 +1,5 @@
 python manage.py makemigrations
 python manage.py migrate
 # python manage.py collectstatic --noinput
+python manage.py loaddata dumped_data.json
 python manage.py runserver 0.0.0.0:8000
